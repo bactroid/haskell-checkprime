2b356df3a5e0a88af674ec2d06af31246cae62d3
Kate Haskell <fuzzcat@bactroid.net>
2017-06-16 11:08:11 -0400

    Switched functions to a more generic Integral typeclass

--- checkprime.hs
+++ checkprime.hs
@@ -1,17 +1,17 @@
 import System.Environment
 import Data.List
 
-factor :: Integer -> [Integer]
+factor :: (Integral a) => a -> [a]
 factor n = [x | x <- [2..n - 1], n `mod` x == 0]
 
-isPrime :: Integer -> Bool
+isPrime :: (Integral a) => a -> Bool
 isPrime n = null $ factor n
 
-add1AndSelf :: Integer -> [Integer] -> [Integer]
+add1AndSelf :: (Integral a) => a -> [a] -> [a]
 add1AndSelf n (x:xs) = 1:n:(x:xs)
 add1AndSelf n [] = 1:n:[]
 
-findFactors :: Integer -> [Integer]
+findFactors :: (Integral a) => a -> [a]
 findFactors n = sort(add1AndSelf n (factor n))
 
 -- This throws an exception when input is invalid. Need to fix that.
