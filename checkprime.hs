428b307f4ac2d1e877a7df1ea8383bc2be4a945f
Kate Haskell <khaskell@ufl.edu>
2017-06-21 08:42:34 -0400

    Now handles invalid input gracefully

--- checkprime.hs
+++ checkprime.hs
@@ -24,8 +24,12 @@ parseArgs = stringToInt . head
 main = do
   args <- getArgs
   let n = parseArgs args
-  if fromJust $ fmap isPrime n
-    then putStrLn $ (show $ fromJust n) ++ " is prime"
-    else do 
-      putStrLn $ (show $ fromJust n) ++ " is composite"
-      putStrLn . show . fromJust $ fmap findFactors n
+  let resultIsPrime = fmap isPrime n
+  if resultIsPrime == Nothing
+    then putStrLn "Please provide a valid postive integer."
+    else do
+      if fromJust resultIsPrime
+        then putStrLn $ (show $ fromJust n) ++ " is prime"
+        else do 
+          putStrLn $ (show $ fromJust n) ++ " is composite"
+          putStrLn . show . fromJust $ fmap findFactors n
