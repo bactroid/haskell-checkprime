37ef92d76f64e7af1b1cb0736352c4bb720b5ebf
Kate Haskell <fuzzcat@bactroid.net>
2017-12-04 07:01:46 -0500

    For isPrime we now only check up to sqrt n as a max

--- Prime.hs
+++ Prime.hs
@@ -8,8 +8,17 @@ import Data.List
 factor :: (Integral a) => a -> [a]
 factor n = [x | x <- [2..n - 1], n `mod` x == 0]
 
+fastFactors :: (Integral a) => a -> [a]
+fastFactors n = [x | x <- [2..intSqrt n], n `mod` x == 0]
+
+intSqrt :: (Integral a) => a -> a
+intSqrt = ceiling . sqrt . fromIntegral
+
 isPrime :: (Integral a) => a -> Bool
-isPrime n = null $ factor n
+isPrime 0 = False
+isPrime 1 = False
+isPrime 2 = True
+isPrime n = null $ fastFactors n
 
 add1AndSelf :: (Integral a) => a -> [a] -> [a]
 add1AndSelf n (x:xs) = 1:n:(x:xs)
@@ -17,4 +26,3 @@ add1AndSelf n [] = 1:n:[]
 
 findFactors :: (Integral a) => a -> [a]
 findFactors n = sort(add1AndSelf n (factor n))
-
