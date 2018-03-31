7f5235ea9862cdacd3d328ee987a7e4cc3c95c2d
Kate Haskell <fuzzcat@bactroid.net>
2018-03-31 09:18:37 -0400

    Simplified code in add1AndSelf

--- src/Prime.hs
+++ src/Prime.hs
@@ -3,7 +3,7 @@ module Prime (
   findFactors
 ) where
 
-import Data.List
+import           Data.List
 
 factor :: (Integral a) => a -> [a]
 factor n = [x | x <- [2..n - 1], n `mod` x == 0]
@@ -22,7 +22,7 @@ isPrime n = null $ fastFactors n
 
 add1AndSelf :: (Integral a) => a -> [a] -> [a]
 add1AndSelf n (x:xs) = 1:n:(x:xs)
-add1AndSelf n [] = 1:n:[]
+add1AndSelf n []     = [1, n]
 
 findFactors :: (Integral a) => a -> [a]
 findFactors n = sort(add1AndSelf n (factor n))
