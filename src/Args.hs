100cbc0cc62013dbffc07ae6adc70515d90e95e0
Kate Haskell <khaskell@ufl.edu>
2018-05-07 10:32:48 -0400

    Added code to handle condition where no arg is provided

--- src/Args.hs
+++ src/Args.hs
@@ -3,14 +3,20 @@ module Args (
 ) where
 
 import           Text.Read
+import           Control.Monad
 
 type Error = String
 
 stringToInt :: String -> Maybe Integer
 stringToInt x = readMaybe x :: Maybe Integer
 
+maybeHead :: [String] -> Maybe String
+maybeHead [] = Nothing
+maybeHead xs = Just $ head xs
+
 parseArgs :: [String] -> Either Error Integer
-parseArgs = maybeToEither . stringToInt . head
+parseArgs = maybeToEither . maybeArgs
+  where maybeArgs = stringToInt <=< maybeHead
 
 maybeToEither :: Maybe a -> Either Error a
 maybeToEither Nothing  = Left "Please provide a valid postive integer."
